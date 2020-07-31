8cd030a4e13a6bef628bed5b5910fe0e017a2105
Zhixing Zhang <me@neoto.xin>
2020-07-30 23:23:06 -0400

    Linking & Basic sanity tests

--- build.rs
+++ build.rs
@@ -5,6 +5,16 @@ use std::path::PathBuf;
 fn main() {
     println!("cargo:rerun-if-changed=wrapper.hpp");
 
+    // Static Links
+    println!("cargo:rustc-link-lib=static=openvdb");
+
+    // Dynamic Links
+    println!("cargo:rustc-link-lib=dylib=c++");   // stdlib
+    println!("cargo:rustc-link-lib=dylib=blosc"); // Blosc
+    println!("cargo:rustc-link-lib=dylib=tbb");   // TBB::tbb
+    println!("cargo:rustc-link-lib=dylib=Half");  // ilmbase::Half
+    println!("cargo:rustc-link-lib=dylib=z");     // ZLIB
+
     // Build components: https://www.openvdb.org/documentation/doxygen/build.html#buildComponents
     let openvdb_path = PathBuf::from(cmake::Config::new("openvdb")
         .define("OPENVDB_BUILD_CORE", "ON")
@@ -20,14 +30,16 @@ fn main() {
         .define("OPENVDB_BUILD_DOCS", "OFF")
         .build());
 
+    // Include the compiled libopenvdb static lib in the search path
+    println!("cargo:rustc-link-search={}", openvdb_path.join("lib").display());
 
     let bindings = bindgen::Builder::default()
         .header("wrapper.hpp")
         .clang_args(&["-x", "c++"]) // Force c++ on .h header files
         .clang_arg("-std=c++17")
         .clang_arg(format!("-I/{}", openvdb_path.join("include").display())) // Include OpenVDB header files
-        .whitelist_recursively(false)
-        .whitelist_type("openvdb::.*::tree::.*")
+        .whitelist_recursively(true)
+        .whitelist_function("openvdb::.*::initialize")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
         .expect("Unable to generate OpenVDB bindings");
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,16 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn initialize() {
+        unsafe {
+            openvdb_v7_1_initialize();
+        }
     }
 }
